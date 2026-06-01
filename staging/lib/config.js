225bc1856925eecdb19587d446e26089540b6cf0
Ito0610 <yutaka.ito1994@gmail.com>
2026-06-01 21:58:39 +0900

    Update config.js

--- staging/lib/config.js
+++ staging/lib/config.js
@@ -1,5 +1,5 @@
-export const SUPABASE_URL = '★検証用 Supabase URL を入力★';
-export const SUPABASE_ANON_KEY = '★検証用 anon key を入力★';
-export const GAS_WEB_APP_URL = '★検証用 GAS Web App URL を入力★';
+export const SUPABASE_URL = 'https://xkclgpfwrjhttpjhexuy.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhrY2xncGZ3cmpodHRwamhleHV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5OTMyMTUsImV4cCI6MjA5NTU2OTIxNX0.it_i5pUErvuthqIoQaY37asLO5wGXGi6ZhNfeiMu4XY';
+export const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycby2pAOagNuqOqpZKDls4gJorDdDz_9sqLZ3mAq4uvHngfGMuyZ0KUoxFENchhH4KfS7LQ/exec';
 export const PASSWORD_HASH = '9af15b336e6a9619928537df30b2e6a2376569fcf9d7e773eccede65606529a0';
 export const APPROVED_BY = '管理者（検証）';
